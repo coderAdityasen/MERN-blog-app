db67f7125c33556256b8bd1bfc4c395a9259942f
coderAdityasen <adityasen561@gmail.com>
2024-03-16 11:04:46 +0530

    import errro resolve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import {BrowserRouter , Routes , Route} from "react-router-dom"
-import About from './Pages/About'
-import Home from './Pages/Home'
-import Signin from './Pages/Signin'
-import SignUp from './Pages/signUp'
-import Search from "./Pages/Search"
-import Projects from "./Pages/Projects"
-import PostPages from "./Pages/PostPages"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import SignUp from "./Pages/SignUp"
+import Signin from "./Pages/Signin"
 import Dashboard from "./Pages/Dashboard"
+
 
 function App() {
   return (
@@ -16,13 +14,12 @@ function App() {
     {/*   <ScrollToTop /> */}
       {/* <Header /> */}
       <Routes>
-        <Route path='/' element={<Home />} />
+       <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<Signin />} />
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
     </>
   )
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
- import React from 'react'
- 
- function Home() {
-   return (
-	 <div>
-	   Home
-	 </div>
-   )
- }
- 
- export default Home
+import React from 'react'
+
+function Home() {
+	return (
+		<>
+		Home
+		</>
+	)
+}
+
+export default Home
