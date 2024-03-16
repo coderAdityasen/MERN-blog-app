119062ec7867220f61247eba9784cc911f8008cf
coderAdityasen <adityasen561@gmail.com>
2024-03-16 10:56:51 +0530

    pages and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import SignUp from './Pages/signUp'
+import Search from "./Pages/Search"
+import Projects from "./Pages/Projects"
+import PostPages from "./Pages/PostPages"
+import Dashboard from "./Pages/Dashboard"
 
 function App() {
   return (
     <>
-    
+    <BrowserRouter>
+    {/*   <ScrollToTop /> */}
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
---
+++ client/src/Pages/Signin.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+function Signin() {
+   return (
+	 <div>
+	   singin
+	 </div>
+   )
+ }
+ 
+ export default Signin
